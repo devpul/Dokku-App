59cf5e7df9b8f1d9db17d8c6b3f98353af739f6e
ibnubachdar <ibnubachdar@gmail.com>
2026-03-29 16:39:17 +0700

    feat(dashboard): add header component (DRY)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,19 +1,13 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
 
-@section('content')
+@section('header', 'Dashboard')
 
+@section('content')
         {{-- RIGHT --}}
         <div class="px-8 py-5 grid grid-rows-[50px_1fr] md:bg-slate-50 ">
-                <div class="flex justify-between items-center">
-                        <div class="flex gap-x-1">
-                            <p class="text-gray-400 font-medium">Dokku ></p>
-                            <p class="font-medium">Dashboard</p>
-                        </div>
-                        <svg class="size-10 bg-blue-100 p-2 rounded-full text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" >
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                        </svg>
-                </div>
+                @include('components.header')
+
                 <div class="flex flex-col gap-y-10">
                     <div class="flex justify-between items-center mt-8">
                         <div>
