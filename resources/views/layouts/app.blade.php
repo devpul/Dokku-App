f9f877138bd719b959eee73c079a4aa3c03d9d47
ibnubachdar <ibnubachdar@gmail.com>
2026-03-29 16:27:26 +0700

    change details: 1. add sidebar component 2. add flash-messages component for session alert 3. setup base layout as master layout (DRY) 4. setup app layout for general navigation 5. setup auth layout for authentication pages 6. implement Inter font as default

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Selamat Datang') | {{ config('app.name') }}</title>
+@extends('layouts.base')
 
-    @vite('resources/css/app.css')
-</head>
-<body>
-    @if (session('success'))
-        <p class="text-center bg-green-400 text-white">{{ session('success') }}</p>
-    @elseif (session('error'))
-        <p class="text-center bg-red-500 text-white">{{ session('error') }}</p>
-    @endif
-
-    @yield('content')
-</body>
-</html>
+@section('main')
+   @extends('components.flash-messages')
+    <div class="min-h-screen grid lg:grid-cols-[250px_1fr] md:grid-cols-[30%_1fr] md:bg-white bg-[#f6f6ff]">
+        {{-- LEFT --}}
+        @include('components.sidebar')
+        @yield('content')
+    </div>
+@endsection
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.base')
+
+@section('main')
+    @extends('components.flash-messages')
+        @yield('content')
+@endsection
